27f0e862c75157a0a2c18d2fcd2909d015f5f93d
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2019-09-26 13:43:44 +0100

    Remove lerna, instead link local dependencies using yarn (#166)
    * Remove lerna, and just use yarn with relative paths instead for simplicity
    
    * bump version

--- scripts/bump.js
+++ scripts/bump.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
-const topLerna = require(path.join(__dirname, '../lerna.json'));
+const topPackage = require(path.join(__dirname, '../lerna.json'));
 const getPackagePath = folder => path.join(__dirname, '../', folder, 'package.json');
 
 console.log('Current versions:');
 
-const packages = topLerna.packages.map((folder) => {
+const packages = topPackage.packages.map((folder) => {
   const packagePath = getPackagePath(folder);
   const packageJson = require(packagePath);
   return {
@@ -28,16 +28,6 @@ rl.question('>> Enter new version for all packages: ', (version) => {
 
   packages.forEach(({ packagePath, packageJson }) => {
     packageJson.version = version;
-
-    packages.forEach(({ name }) => {
-      if (packageJson.devDependencies && (name in packageJson.dependencies)) {
-        packageJson.dependencies[name] = `^${version}`;
-      }
-      if (packageJson.devDependencies && (name in packageJson.devDependencies)) {
-        packageJson.devDependencies[name] = `^${version}`;
-      }
-    });
-
     fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
     console.log(`${packageJson.name}\t updated to ${packageJson.version}`);
   });
