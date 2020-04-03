a5fbce209bb8ef3ddc7905e8dd7c909d1844b6b9
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2020-04-03 13:35:52 +0100

    Store spread gain adjustments in dB (#297)
    * Simplify gain input to allow any number to be entered (without conversion to decibels)
    
    * bump version

--- react-frontend/src/Components/Project/Objects/Sequence/BehaviourParameters/GainInput.jsx
+++ react-frontend/src/Components/Project/Objects/Sequence/BehaviourParameters/GainInput.jsx
@@ -1,68 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Input,
 } from 'semantic-ui-react';
 
-// Convert between linear gain unit (stored in metadata) and dB (displayed to user) as per WebAudio
-// spec: https://webaudio.github.io/web-audio-api/#linear-to-decibel
-const linearGainToDecibels = v => (v === 0 ? -1000 : 20 * Math.log10(v));
-const decibelsToLinearGain = v => 10 ** (v / 20);
-
 // helper to validate that the input string is a valid number
 const stringIsNumber = value => !Number.isNaN(parseFloat(value));
 
-class GainInput extends React.PureComponent {
-  constructor(props) {
-    super(props);
+const GainInput = ({
+  value,
+  onChange,
+}) => {
+  const [error, setError] = useState();
 
-    const { value } = props;
-
-    this.state = {
-      error: false,
-      displayValue: linearGainToDecibels(value).toFixed(1),
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e, data) {
-    const { onChange } = this.props;
+  const handleChange = (e, data) => {
     const { value: inputValue } = data;
 
     // sanitize input value by removing non-numeric characters; can't simply use parseFloat because
     // the user might be in the middle of typing a negative or decimal.
-    const displayValue = inputValue.replace(/[^0-9\-+.]/, '');
-    const error = !stringIsNumber(displayValue);
-    this.setState({
-      displayValue,
-      error,
-    });
+    const newValue = inputValue.replace(/[^0-9\-+.]/, '');
+    const newError = !stringIsNumber(newValue);
+    setError(newError);
 
-    // validate the display value and pass it to the onChange handler only if it is valid.
-    // if it is not valid, save the default value of unity gain instead.
-    const value = error ? 1.0 : decibelsToLinearGain(parseFloat(displayValue));
-    onChange(e, { value });
-  }
+    // validate the input value and pass it to the onChange handler only if it is valid.
+    if (!newError) {
+      onChange(e, {
+        value: parseFloat(newValue),
+      });
+    }
+  };
 
-  render() {
-    const {
-      error,
-      displayValue,
-    } = this.state;
-
-    return (
-      <Input
-        placeholder="Gain"
-        label={{ content: 'dB', basic: true }}
-        labelPosition="right"
-        error={error}
-        value={displayValue}
-        onChange={this.handleChange}
-      />
-    );
-  }
-}
+  return (
+    <Input
+      placeholder="Gain"
+      label={{ content: 'dB', basic: true }}
+      labelPosition="right"
+      defaultValue={value}
+      error={error}
+      onChange={handleChange}
+    />
+  );
+};
 
 GainInput.propTypes = {
   value: PropTypes.number.isRequired,
