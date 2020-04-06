87efbb9535ba87e8f2622c0dcfbf10dd044d14a8
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2020-04-06 17:24:01 +0100

    Add additional ffmpeg search path so we can use a different binary than the system installation (#301)
    * Add custom path to which, so that we can use ffmpeg outside of a system path
    
    * bump version

--- background-tasks/src/which.js
+++ background-tasks/src/which.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import os from 'os';
 import { promisify } from 'util';
 import { exec as execCB } from 'child_process';
 import { realpath as realpathCB } from 'fs';
@@ -6,6 +8,14 @@ import { getLogger } from 'bbcat-orchestration-builder-logging';
 const logger = getLogger('which');
 const exec = promisify(execCB);
 const realpath = promisify(realpathCB);
+
+// TODO macOS/Linux specific
+const searchPath = [
+  path.join(os.homedir(), 'bbcat-orchestration-builder-ffmpeg', 'bin'),
+  process.env.PATH,
+  '/usr/bin',
+  '/usr/local/bin',
+].join(':');
 
 const results = {};
 
@@ -17,18 +27,18 @@ const which = (name) => {
   return exec(`which ${name}`, {
     env: {
       ...process.env,
-      PATH: `${process.env.PATH}:/usr/bin:/usr/local/bin`, // TODO macOS/Linux specific
+      PATH: searchPath,
     },
   })
     .then(({ stdout }) => {
       logger.silly(`which ${name} returned ${stdout}`);
       return stdout.trim();
     })
-    .then(path => realpath(path))
-    .then((path) => {
-      logger.silly(`realpath on which result for ${name} returned ${path}`);
-      results[name] = path;
-      return path;
+    .then(execPath => realpath(execPath))
+    .then((realExecPath) => {
+      logger.silly(`realpath on which result for ${name} returned ${realExecPath}`);
+      results[name] = realExecPath;
+      return realExecPath;
     })
     .catch((err) => {
       logger.error(`Could not find ${name} (running 'which ${name}' or resolving real path failed: ${err})`);
